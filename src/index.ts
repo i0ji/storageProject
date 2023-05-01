9179bf6160a8581eb3e12452a6e2fee163ca5a07
Ioann <108623902+i0ji@users.noreply.github.com>
2023-05-01 23:26:30 +0300

    invoice field emptyCheck func added

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,5 @@ const inputField = document.getElementById('invoice') as HTMLInputElement;
 const button = document.getElementById('add_btn') as HTMLButtonElement;
 
 inputField.addEventListener('keyup', () => {
-    if (inputField.value.trim() === '') {
-        button.disabled = true;
-    } else {
-        button.disabled = false
-    }
+    button.disabled = inputField.value.trim() === '';
 })
